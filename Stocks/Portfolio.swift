05101a88caf60e339062d4b8cb1b1b881b7d877a
Sven Günther <sven.guenther@it-agile.de>
2021-01-26 08:17:54 +0100

    move methods to portfolio

--- Stocks/Portfolio.swift
+++ Stocks/Portfolio.swift
@@ -6,6 +6,28 @@ struct Portfolio: Codable {
     init(depots: [Depot] = []) {
         self.depots = depots
     }
+    
+    mutating func addDepot(named name: String, withCash cash: String) {
+        let depot = Depot(name: name, initialBalance: Money(from: cash))
+        depots.append(depot)
+    }
+    
+    mutating func deleteDepots(atIndexSet indexSet: IndexSet) {
+        indexSet.forEach { index in
+            depots.remove(at: index)
+        }
+    }
+    
+    mutating func addCashTransaction(_ transaction: CashTransaction, toDepot depot: Depot) {
+        guard let depotIndex = depots.firstIndex(where: { $0.id == depot.id }) else { return }
+        depots[depotIndex].addCashTransaction(transaction)
+    }
+
+    mutating func addOrderTransaction(_ transaction: OrderTransaction, toDepot depot: Depot) {
+        guard let depotIndex = depots.firstIndex(where: { $0.id == depot.id }) else { return }
+        depots[depotIndex].addOrderTransaction(transaction)
+    }
+
 }
     
     
--- Stocks/StateController.swift
+++ Stocks/StateController.swift
@@ -10,27 +10,22 @@ class StateController: ObservableObject {
     }
     
     func addDepot(named name: String, withCash cash: String) {
-        let depot = Depot(name: name, initialBalance: Money(from: cash))
-        portfolio.depots.append(depot)
+        portfolio.addDepot(named: name, withCash: cash)
         storageController.save(portfolio)
     }
     
     func deleteDepots(atIndexSet indexSet: IndexSet) {
-        indexSet.forEach { index in
-            portfolio.depots.remove(at: index)
-        }
+        portfolio.deleteDepots(atIndexSet: indexSet)
         storageController.save(portfolio)
     }
     
     func addCashTransaction(_ transaction: CashTransaction, toDepot depot: Depot) {
-        guard let depotIndex = portfolio.depots.firstIndex(where: { $0.id == depot.id }) else { return }
-        portfolio.depots[depotIndex].addCashTransaction(transaction)
+        portfolio.addCashTransaction(transaction, toDepot: depot)
         storageController.save(portfolio)
     }
 
     func addOrderTransaction(_ transaction: OrderTransaction, toDepot depot: Depot) {
-        guard let depotIndex = portfolio.depots.firstIndex(where: { $0.id == depot.id }) else { return }
-        portfolio.depots[depotIndex].addOrderTransaction(transaction)
+        portfolio.addOrderTransaction(transaction, toDepot: depot)
         storageController.save(portfolio)
     }
 
