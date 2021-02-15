2ab5f258fae061ee476b4ae1067e743d57727805
Sven Günther <sven.guenther@it-agile.de>
2021-02-15 17:01:31 +0100

    refactor: remove unused method

--- Stocks/Finance/FinanceAPI.swift
+++ Stocks/Finance/FinanceAPI.swift
@@ -8,16 +8,11 @@ struct SecurityQuotes {
 }
 
 protocol FinanceAPIProtocol {
-    func getQuotesForSymbol(symbol: String, completion: @escaping (Result<SecurityQuotes, NetworkError>)->())
     func getQuotesForSymbols(symbols: [String], completion: @escaping (Result<[SecurityQuotes], NetworkError>)->())
 }
 
 struct FinanceAPI: FinanceAPIProtocol {
     static var shared: FinanceAPIProtocol = YahooFinanceAPI()
-
-    func getQuotesForSymbol(symbol: String, completion: @escaping (Result<SecurityQuotes, NetworkError>) -> ()) {
-        Self.shared.getQuotesForSymbol(symbol: symbol, completion: completion)
-    }
 
     func getQuotesForSymbols(symbols: [String], completion: @escaping (Result<[SecurityQuotes], NetworkError>) -> ()) {
         Self.shared.getQuotesForSymbols(symbols: symbols, completion: completion)
--- Stocks/Finance/YahooFinanceAPI.swift
+++ Stocks/Finance/YahooFinanceAPI.swift
@@ -34,27 +34,6 @@ struct YahooFinanceAPI: FinanceAPIProtocol {
         }
     }
 
-    func getQuotesForSymbol(symbol: String, completion: @escaping (Result<SecurityQuotes, NetworkError>) -> ()) {
-        let queryParameters = [
-            QueryParameter(parameter: "symbols", value: symbol)
-        ]
-        let allQueryParameters = queryParameters + regionParameters
-
-        let url = "https://yahoo-finance-low-latency.p.rapidapi.com/v6/finance/quote"
-        Webservice.shared.loadResource(url: url, headerFields: headerFields, queryParameter: allQueryParameters) { (result:Result<YahooQuoteAPIResponse, NetworkError>) in
-            switch result {
-            case .success(let response):
-                if let quote = response.quoteResponse.result.filter({ $0.symbol == symbol }).first {
-                    completion(.success(SecurityQuotes(symbol: symbol, currentPrice: Money(amount: quote.regularMarketPrice), changePercentage: quote.regularMarketChangePercent, changeAbsolute: Money(amount: quote.regularMarketChange))))
-                } else {
-                    completion(.failure(.badRequest))
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-
     func getKeyFor(name: String) -> String? {
         if let url = Bundle.main.url(forResource: "api-keys", withExtension: "plist") {
             do {
